abad10f05a18105d2bf9111d1095746e9fcaee1b
Vedansh Kamdar <vedansh.kamdar1413@gmail.com>
2023-08-26 23:34:25 +0530

    added question

--- footbal.java
+++ footbal.java
@@ -1,27 +1,27 @@
-// 43A Football
 import java.util.Scanner;
 
 public class footbal {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         sc.nextLine();
-        String s=sc.nextLine();
-        int count1=1,count2=0;
-        String m="";
-        String arr[]=new String[n-1];
-        for(int i=0;i<n-1;i++){
-            arr[i]=sc.nextLine();
-            if(arr[i].equals(s))
-            count1++;
-            else
-            m=arr[i];
-            count2++;
+        String s = sc.nextLine();
+        int count1 = 1, count2 = 0;
+        String m = "";
+        String arr[] = new String[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = sc.nextLine();
+            if (arr[i].equals(s))
+                count1++;
+            else {
+                m = arr[i];
+                count2++;
             }
-            if(count1>count2)
-            System.out.println(m);
-            else
+        }
+        if (count1 > count2)
             System.out.println(s);
+        else
+            System.out.println(m);
         sc.close();
     }
 }
