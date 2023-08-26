ca4fa5147518f7b6390455373ed15520aca9b6a7
Vedansh Kamdar <vedansh.kamdar1413@gmail.com>
2023-08-27 00:27:32 +0530

    added questions

--- helpful_maths.java
+++ helpful_maths.java
@@ -2,29 +2,33 @@ import java.util.Scanner;
 
 public class helpful_maths {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String st=sc.nextLine();
-        int n=st.length()/2+1;
-        int temp=0;
-        int [] arr=new int[(st.length()/2)+1];
-        for(int i=0;i<st.length();i=i+2)
-        {
-            arr[i]=st.charAt(i);
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        int n = (st.length() + 1) / 2;
+        int temp = 0;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < st.length(); i = i + 2) {
+            arr[i / 2] = st.charAt(i) - '0'; 
         }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=1;j<n-i;j++)
-            {
-                if(arr[j-1]>arr[j])
-                temp = arr[j-1];  
-                arr[j-1] = arr[j];  
-                arr[j] = temp; 
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n - i; j++) {
+                if (arr[j - 1] > arr[j]) {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
             }
         }
-        for(int i=0;i<arr.length;i++)
-        {
-            System.out.println(arr[i]+"+");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i != arr.length - 1) {
+                System.out.print("+");
+            }
         }
+        
         sc.close();
     }
 }
--- twin.java
+++ twin.java
@@ -5,14 +5,23 @@ public class twin {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int count=0;
-        int [] arr=new int[n];
+        int arr[]=new int[n];
+        int sum=0;
         for(int i=0;i<n;i++)
         {
-            arr[i]=sc.nextInt();
-            count=count+arr[i];
+            sum=sum+arr[i];
         }
-        
-
+        Arrays.sort(arr);
+        int t=0;
+        int c=0;
+        for (int i = n - 1; i >= 0; i--) {
+            c=c+ arr[i];
+            t++;
+            if (c >= t / 2) {
+                break;
+            }
+        }
+        System.out.println(t);
+        sc.close();
     }
 }
